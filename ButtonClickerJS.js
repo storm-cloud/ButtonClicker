1783906c932723ec9d1d25bd94216ac04c1ec543
storm-cloud <nicholas_tilton@icloud.com>
2022-05-30 14:16:07 -0400

    Rough af

---
+++ ButtonClickerJS.js
@@ -0,0 +1,11 @@
+function removeButt(){
+    addeff.remove();
+}
+
+function login(){
+    document.getElementById('log').innerText="Logout"
+}
+
+function likealert(){
+    alert("Ninja was liked");
+}
